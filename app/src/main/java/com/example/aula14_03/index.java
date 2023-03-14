08ea0b7a8390b78cc35419545e5999ce4ff55355
lab <user@IFPR-Lab.localdomain>
2023-03-14 19:53:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula14_03/index.java
@@ -0,0 +1,14 @@
+package com.example.aula14_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
